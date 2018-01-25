e88b569f8510ad2f0ab8b1696baf2ca2591da142
wagenhuber <wagenhuber@gmx.net>
2018-01-25 10:13:15 +0100

    Verschlüsselung ok Entschlüsselung nicht ok

--- JPasswortBunker/src/model/Testklasse.java
+++ JPasswortBunker/src/model/Testklasse.java
@@ -1,22 +1,32 @@
 package model;
 
-import javax.crypto.*;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class Testklasse {
 
-    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
+    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, UnsupportedEncodingException {
+
+        SecretKeySpec secretKeySpec;
+        byte[] key;
 
         StringBuilder stringBuilder = new StringBuilder();
         String passwort = "geheim";
         int opmode = 1;
 
         //Erzeugt ein KeyObject
-        Key key = new SecretKeySpec("geheim".getBytes(), "DES");
+        key = passwort.getBytes("UTF-8");
+        key = Arrays.copyOf(key, 16);
+        secretKeySpec = new SecretKeySpec(key, "AES");
 
 
 
@@ -47,19 +57,33 @@ public class Testklasse {
          * Verschlüsselung mit Cipher
          */
 
-        Cipher cipher = Cipher.getInstance("DES");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        System.out.println("________________Verschlüsselung_____________");
+
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
         byte[] encryptedDigest = cipher.doFinal(digest);
         System.out.println(cipher.getParameters());
 
+        String verschlüsselt = Base64.getEncoder().encodeToString(encryptedDigest);
+
+
+        System.out.println("Verschlüsselte Daten im ByteArray:");
+        System.out.println(encryptedDigest);
+        System.out.println("Verschlüsselte Daten als String:");
+        System.out.println(verschlüsselt);
 
         /**
          * Entsclüsselung
          */
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] decryptedDigest = cipher.doFinal(digest);
+        System.out.println("________________Entschlüsselung_____________");
 
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        //byte[] decryptedDigest = cipher.doFinal(digest);
+        byte[] entschlüsseltBytes = cipher.doFinal(encryptedDigest);
+        String entschlüsselt = Base64.getEncoder().encodeToString(entschlüsseltBytes);
 
+        System.out.println(entschlüsselt);
     }//ende main
 
 }
