d5e22d6b16eea2a191c1a5006a15a03d19ce305a
Michael Kopp <michi_kopp1@web.de>
2018-02-05 15:30:10 +0100

    EntryDB addEntry, equals, hashCode und toString eingefügt

--- JPasswortBunker/src/jpasswortbunker/mgm/entry/EntryDB.java
+++ JPasswortBunker/src/jpasswortbunker/mgm/entry/EntryDB.java
@@ -3,6 +3,8 @@ package jpasswortbunker.mgm.entry;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.Objects;
+
 public class EntryDB {
 
     private ObservableList<Entry> entryList;
@@ -15,5 +17,34 @@ public class EntryDB {
         entryList.addAll(entry);
     }
 
+    public void addEntry(String title, String username, String password, String url,
+                         String description, int categorieID) {
+        entryList.add(new Entry(title, username, password, url, description, categorieID));
+    }
 
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EntryDB entryDB = (EntryDB) o;
+        return Objects.equals(entryList, entryDB.entryList);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(entryList);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("All Entrys:");
+
+        for (Entry entry:entryList) {
+            sb.append(entry.toString());
+        }
+        return sb.toString();
+    }
 }
