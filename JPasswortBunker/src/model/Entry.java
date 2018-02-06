964e46ca39f501f8480b88e4df2c0ed2b0ecb52d
wagenhuber <wagenhuber@gmx.net>
2018-02-06 09:03:16 +0100

    Klasse Entry erweitert, Klasse EntryService erweitert

--- JPasswortBunker/src/model/Entry.java
+++ JPasswortBunker/src/model/Entry.java
@@ -18,7 +18,78 @@ public class Entry {
         this.categoryID = categoryID;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    private void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    private void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getDbID() {
+        return dbID;
+    }
+
+    private void setDbID(int dbID) {
+        this.dbID = dbID;
+    }
+
+    public int getEntryID() {
+        return entryID;
+    }
+
+    private void setEntryID(int entryID) {
+        this.entryID = entryID;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    private void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public void updateEntry(String title, String url, String description, String username, String password){
+        this.setTitle(title);
+        this.setUrl(url);
+        this.setDescription(description);
+        this.setUsername(username);
+        this.setPassword(password);
 
 
+    }
 
 }
--- JPasswortBunker/src/model/EntryService.java
+++ JPasswortBunker/src/model/EntryService.java
@@ -2,4 +2,19 @@ package model;
 
 public class EntryService {
 
+    public Entry newEntry(String title, String description, String url, String username, String password, int dbID, int entryID, int categoryID) {
+        Entry entry = new Entry(title, description, url, username, password, dbID, entryID, categoryID);
+        return entry;
+    }
+
+
+
+
+    public Entry updateEntry(Entry entry) {
+        entry.updateEntry();
+    }
+
+
+
+
 }
