1821e6181f760732373cb84703ae5d489f3a528f
Michael Kopp <michi_kopp1@web.de>
2018-02-21 09:42:48 +0100

    ModelMain angepasst

--- JPasswortBunker/src/jpasswortbunker/mgm/model/ModelMain.java
+++ JPasswortBunker/src/jpasswortbunker/mgm/model/ModelMain.java
@@ -1,19 +1,28 @@
 package jpasswortbunker.mgm.model;
 
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 public class ModelMain {
 
     private EntryList entryList;
-    private String MasterPassword;
+    private PasswordObject masterPassword;
+    private EncryptionService encryptionService;
 
 
 
-    public ModelMain() {
+
+    public ModelMain() throws NoSuchPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException {
         this.entryList = new EntryList();
+        this.encryptionService = new EncryptionService();
+
     }
 
     public void addEntryToList(Entry entry) {
         entryList.addEntry(entry);
     }
+
 
 
 }
